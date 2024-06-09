75f05d6dee553de8cc54ae9fc36f09ae02980c11
richardzmija <zmija.ryszard@protonmail.com>
2024-06-09 13:36:19 +0200

    Completed a GUI for the genre classifier together with a simple audio playback.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == "__main__":
-    pass
+import sys
+from PyQt5.QtWidgets import QApplication
+from src.gui.main_window import MainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
